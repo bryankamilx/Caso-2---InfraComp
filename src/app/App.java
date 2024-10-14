a48938c80a9f3ccb92d5da33ca9de59df0b587a1
bryankamilx <b.orjuelam@uniandes.edu.co>
2024-10-14 17:41:53 -0500

    rutas relativas de archivos

--- src/app/App.java
+++ src/app/App.java
@@ -12,15 +12,14 @@ import utilidades.Temporizador;
 
 public class App {
     public static void main(String[] args) {
+        int tamañoPagina = 256;  // Ajusta el tamaño de página según sea necesario
+        String nombreImagen = "archivos/caso2-parrots.bmp";  // Ruta relativa al archivo de imagen dentro del proyecto
+        String archivoSalida = "archivos/referencias.txt";  // Ruta relativa al archivo de salida dentro del proyecto
 
-        int tamañoPagina = 256;  // Puedes cambiar el tamaño de página según lo necesites
-        String nombreImagen = "C:\\Users\\Bryan\\Documents\\Caso 2 - InfraComp\\Archivos\\caso2-parrots.bmp";
-        String archivoSalida = "C:\\Users\\Bryan\\Documents\\Caso 2 - InfraComp\\Archivos\\referencias.txt";
-
+        // Genera el archivo de referencias
         ArchivoReferencias.generarReferencias(tamañoPagina, nombreImagen, archivoSalida);
 
         System.out.println("Archivo de referencias generado en: " + archivoSalida);
-        
     }
 }
 
