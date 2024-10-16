012d2f1011c01b41b9e92f681b9f65cf5ddc3267
bryankamilx <b.orjuelam@uniandes.edu.co>
2024-10-16 11:35:21 -0500

    Opción 2

--- src/app/App.java
+++ src/app/App.java
@@ -12,14 +12,15 @@ import utilidades.Temporizador;
 
 public class App {
     public static void main(String[] args) {
-        int tamañoPagina = 512;  // Ajusta el tamaño de página según sea necesario
-        String nombreImagen = "archivos/caso2-parrots_mod.bmp";  // Ruta relativa al archivo de imagen dentro del proyecto
-        String archivoSalida = "archivos/referencias.txt";  // Ruta relativa al archivo de salida dentro del proyecto
+        int numMarcos = 4;           // Número de marcos de página
+        int tiempoHit = 25;          // Tiempo en ns para un hit
+        int tiempoFallo = 10;        // Tiempo en ms para un fallo de página
+        String archivoReferencias = "archivos/referencias.txt"; // Archivo de referencias
 
-        // Genera el archivo de referencias
-        ArchivoReferencias.generarReferencias(tamañoPagina, nombreImagen, archivoSalida);
-
-        System.out.println("Archivo de referencias generado en: " + archivoSalida);
+        SimuladorPaginacion simulador = new SimuladorPaginacion(numMarcos, tiempoHit, tiempoFallo);
+        simulador.cargarReferencias(archivoReferencias);
+        simulador.ejecutarSimulacion();
+        simulador.mostrarResultados();
     }
 }
 
